a9c187fa05178d8d8893d4a2d2ad5f95dc16c913
HaydenW397 <61121265+HaydenW397@users.noreply.github.com>
2020-02-16 11:46:38 -0700

    Add files via upload

---
+++ main.c
@@ -0,0 +1,22 @@
+#include <kipr/wombat.h>
+
+int main()
+{
+    printf("Hello World\n");\
+        camera_open_black();
+    camera_update();
+    
+    
+    while (get_object_count(==0);
+    {
+        motor(0,50);
+        motor(3,50);
+    }
+    
+    while (get_object_count(==1);
+    {
+        motor(0,0);
+        motor(3,0);
+    }
+    return 0;
+}
